5b363e8c54b7c48ce7a69cc29f30246ee2c08ec6
Sahil <samyanisahil007@gmail.com>
2022-07-29 13:51:27 +0530

    Links correction

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-  <a className="navbar-brand" href="">{props.title}</a>
+  <a className="navbar-brand" href="#">{props.title}</a>
   {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
@@ -15,7 +15,7 @@ export default function Navbar(props) {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <a className={`nav-link text-${props.mode==='light'?'dark':'light'}`} href="">Home <span className="sr-only"></span></a>
+        <a className={`nav-link text-${props.mode==='light'?'dark':'light'}`} href="#">Home <span className="sr-only"></span></a>
         {/* <Link className={`nav-link text-${props.mode==='light'?'dark':'light'}`} to="/">Home <span className="sr-only"></span></Link> */}
       </li>
       {/* <li className="nav-item">
